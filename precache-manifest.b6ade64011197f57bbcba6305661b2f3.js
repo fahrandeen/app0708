7372c50ec568d10fe679a2f1b0542cc69e2b3408
Fahran Deen <fahrandeen17@gmail.com>
2019-08-13 11:05:56 +0530

    Updates

---
+++ precache-manifest.b6ade64011197f57bbcba6305661b2f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b438ffdce72c13ea743dcf7b04b4c9e",
+    "url": "/app0708/index.html"
+  },
+  {
+    "revision": "71ddf8503413442852d1",
+    "url": "/app0708/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "eb6e0d1e326bd6590f07",
+    "url": "/app0708/static/js/2.eaebbfee.chunk.js"
+  },
+  {
+    "revision": "71ddf8503413442852d1",
+    "url": "/app0708/static/js/main.40e9a770.chunk.js"
+  },
+  {
+    "revision": "a4e3308e55b43df32fa7",
+    "url": "/app0708/static/js/runtime~main.a03244af.js"
+  }
+]);
